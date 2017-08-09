9e134710a66f9e703d7438bf835a0d946d77721d
georgedimmitrov <georgedimmitrov@gmail.com>
2017-08-09 15:39:00 +0300

    Untracked everything

--- handlers/passport.js
+++
@@ -1,9 +0,0 @@
-const passport = require('passport');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-
-passport.use(User.createStrategy());
-
-// every single request if user is logged in we attach the User to the .req object
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
